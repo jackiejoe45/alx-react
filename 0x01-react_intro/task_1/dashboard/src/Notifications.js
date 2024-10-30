294353d6cb7751edfb86874e4de273575c278541
jackiejoe45 <ben.abdul@ashesi.edu.gh>
2024-10-30 17:25:56 +0000

    Completed Task 2

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,18 @@
+/*
+It should import React
+It should export a function
+The function should return a div with the class Notifications
+The div should contain a paragraph with the text Here is the list of notifications
+import the file Notifications.css.
+*/
+
+import React from 'react';
+
+export default function Notifications() {
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
+
