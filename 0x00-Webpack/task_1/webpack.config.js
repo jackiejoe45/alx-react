37e2ead846cdf0f831644b5f8aadb5df719fb41f
jackiejoe45 <ben.abdul@ashesi.edu.gh>
2024-11-01 21:15:19 +0000

    Corrections

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
+    filename:  "bundle.js",
     path: path.resolve(__dirname, 'public')
   },
   mode: 'production'
